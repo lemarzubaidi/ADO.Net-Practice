e99e28ca097192bfa05306f3c7fd14e1dd54c099
Lemar Zubaidi <lemarzubaide9@gmail.com>
2026-07-08 22:08:42 +0300

    Add files via upload

---
+++ DEleteContactss.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static string connectionstring = "Server=.;Database=ContactsDB;User Id=sa;Password=sa123456;";
+        static void Deletecontacts(string ContactIDs)
+        {
+            SqlConnection connection = new SqlConnection(connectionstring);
+            string query = @"Delete FROM Contacts
+                                      where ContactID in ("+ContactIDs +")";
+
+            SqlCommand command = new SqlCommand(query, connection);
+    
+            try
+            {
+                connection.Open();
+                int rowAffected = command.ExecuteNonQuery();
+                if (rowAffected > 0)
+                {
+                    Console.WriteLine("Record Delete successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Record deletion failed");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Deletecontacts("1,2");
+
+        }
+    }
+}
